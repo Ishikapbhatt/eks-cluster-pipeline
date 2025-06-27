8fba86f6c34447675190445d5cd3b87c2e76b906
Ishika Bhatt <ishikap1998@gmail.com>
2025-06-27 09:16:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,113 +2,31 @@ provider "aws" {
   region = "us-west-2"
 }
 
-# VPC
-resource "aws_vpc" "eks_vpc" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_support   = true
-  enable_dns_hostnames = true
-  tags = {
-    Name = "eks-vpc"
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "4.0.2"
+  name    = "eks-vpc"
+  cidr    = "10.0.0.0/16"
+  azs     = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  private_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  enable_nat_gateway = true
+}
+
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = "my-eks-cluster"
+  cluster_version = "1.28"
+  subnets         = module.vpc.private_subnets
+  vpc_id          = module.vpc.vpc_id
+
+  eks_managed_node_groups = {
+    default = {
+      desired_capacity = 2
+      max_capacity     = 3
+      min_capacity     = 1
+
+      instance_types = ["t3.medium"]
+    }
   }
-}
-
-# Subnets
-resource "aws_subnet" "eks_subnet" {
-  count                  = 2
-  vpc_id                 = aws_vpc.eks_vpc.id
-  cidr_block             = "10.0.${count.index}.0/24"
-  map_public_ip_on_launch = true
-  availability_zone      = data.aws_availability_zones.available.names[count.index]
-
-  tags = {
-    Name = "eks-subnet-${count.index}"
-  }
-}
-
-data "aws_availability_zones" "available" {}
-
-# Internet Gateway
-resource "aws_internet_gateway" "eks_igw" {
-  vpc_id = aws_vpc.eks_vpc.id
-
-  tags = {
-    Name = "eks-igw"
-  }
-}
-
-# Route Table
-resource "aws_route_table" "eks_route_table" {
-  vpc_id = aws_vpc.eks_vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.eks_igw.id
-  }
-
-  tags = {
-    Name = "eks-route-table"
-  }
-}
-
-# Route Table Association
-resource "aws_route_table_association" "eks_rta" {
-  count          = 2
-  subnet_id      = aws_subnet.eks_subnet[count.index].id
-  route_table_id = aws_route_table.eks_route_table.id
-}
-
-# IAM Role for EKS
-resource "aws_iam_role" "eks_cluster_role" {
-  name = "eks-cluster-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = {
-          Service = "eks.amazonaws.com"
-        }
-        Action = "sts:AssumeRole"
-      }
-    ]
-  })
-
-  tags = {
-    Name = "eks-cluster-role"
-  }
-}
-
-# IAM Policy Attachment for EKS Role
-resource "aws_iam_role_policy_attachment" "eks_cluster_policy_attachment" {
-  role       = aws_iam_role.eks_cluster_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-}
-
-resource "aws_iam_role_policy_attachment" "eks_service_policy_attachment" {
-  role       = aws_iam_role.eks_cluster_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
-}
-
-# EKS Cluster
-resource "aws_eks_cluster" "eks_cluster" {
-  name     = "my-eks-cluster"
-  role_arn = aws_iam_role.eks_cluster_role.arn
-
-  vpc_config {
-    subnet_ids = aws_subnet.eks_subnet[*].id
-  }
-
-  tags = {
-    Name = "my-eks-cluster"
-  }
-}
-
-# Output EKS Cluster Information
-output "eks_cluster_endpoint" {
-  value = aws_eks_cluster.eks_cluster.endpoint
-}
-
-output "eks_cluster_arn" {
-  value = aws_eks_cluster.eks_cluster.arn
 }
